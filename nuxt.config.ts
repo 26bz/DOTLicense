21aea915e49692d913e352f4f681fbbd428696a0
26bz <109409095+26bz@users.noreply.github.com>
2025-08-28 09:34:04 -0400

    feat(core): add pinia to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-auth-utils', '@prisma/nuxt', 'nuxt-authorization'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    'nuxt-auth-utils',
+    '@prisma/nuxt',
+    'nuxt-authorization',
+    '@pinia/nuxt',
+  ],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
